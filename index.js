627b75935846e6b786f0adfe7c8df37d1640bec7
Brindia Joseph <brindiatyjuana@gmail.com>
2022-04-07 10:44:08 -0500

    commit tree 546e984d74db5a30ddb5822e21b040b44f395c37

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+// declare variable return first-2-drivers using const 
+//  set array values 
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers=function(){
+    return drivers.slice(0,2)
+};
+const returnLastTwoDrivers=function(){
+    return drivers.slice(-2)
+};
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier= function(multiplyValue){
+    return function(value){
+        return multiplyValue * value;
+    };
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, driversToReturn) => driversToReturn (drivers);
